92011d1e9012c2c66c49dc8d06bb640f9a5a21d7
Juanesph <juanesph@gmail.com>
2023-06-10 03:36:18 -0400

    Creo carpeta JS y subo tarea on click

---
+++ JS/juan_patino_on_click/script.js
@@ -0,0 +1,11 @@
+function change(element) {
+    element.innerText = "Logout"
+}
+
+function hide(element) {
+    element.remove();
+}
+
+function sendMessage(element) {
+    alert("Ninja was liked");
+}
