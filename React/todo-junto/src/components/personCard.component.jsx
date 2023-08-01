c8a6ad15658e91b5a60d75cf2fcf16a483f78dc4
Juanesph <juanesph@gmail.com>
2023-07-31 20:39:45 -0400

    Subiendo todo junto

--- React/todo-junto/src/components/personCard.component.jsx
+++ React/todo-junto/src/components/personCard.component.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 
 const PersonCard = (props) => {
   const { firstName, lastName, age, hairColor } = props;
+  const [currentAge, setCurrentAge] = useState(age);
+
+  const increaseAge = () => {
+    setCurrentAge(currentAge + 1);
+  }
 
   return (
     <div>
       <h2>{lastName}, {firstName}</h2> 
-      <p>Age: {age}</p>
+      <p>Age: {currentAge}</p>
       <p>Hair Color: {hairColor}</p>
+      <button onClick={increaseAge}>Cumpleaños</button>
     </div>
   );
 };
