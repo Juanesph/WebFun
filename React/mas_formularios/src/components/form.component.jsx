00df9bf2613c5dd4b235093f5e21beb279aade72
Juanesph <juanesph@gmail.com>
2023-08-05 21:41:00 -0400

    mas formularios

--- React/mas_formularios/src/components/form.component.jsx
+++ React/mas_formularios/src/components/form.component.jsx
@@ -1,80 +1,118 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-const Form = () => {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-  });
+const Form = (props) => {
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confpassword, setConfPassword] = useState("");
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [passwordError, setPasswordeError] = useState("");
+  const [confpasswordError, setConfPasswordError] = useState("");
+  const [formCreated, setFormCreated] = useState(false);
+
+
+  const handleOnChangeFirstName = (e) => {
+    setFirstName(e.target.value);
+    if (e.target.value.length < 2) {
+      setFirstNameError("Firstname must have 2 characters or longer!");
+    } else {
+      setFirstNameError("");
+    }
+  }
+
+  const handleOnChangeLastName = (e) => {
+    setLastName(e.target.value);
+    if (e.target.value.length < 2) {
+      setLastNameError("lastname must have 2 characters or longer!");
+    } else {
+      setLastNameError("");
+    }
+  }
+
+  const handleOnChangePassword = (e) => {
+    setPassword(e.target.value);
+    if (e.target.value.length < 8) {
+      setPasswordeError("Password must have 8 character or longer!");
+    } else {
+      setPasswordeError("");
+    } 
+  }
+
+  const handleOnChangeConfPassword = (e) => {
+    if (e.target.value !== `${password}`) {
+      setConfPasswordError("Password and confirm password must match");
+    } else {
+      setConfPasswordError("");
+    }
+  }
+
+
+  const handleOnSubmitForm = (e) => {
+    e.preventDefault(); 
+    setFormCreated(true);
   };
-
-  
-
+ 
   return (
-    <div>
-      <h2>Formulario</h2>
-      <form>
-        <div>
-          <label htmlFor="firstName">First Name:</label>
+    <div className="w-50 mt-4 mx-auto">
+      <h1>Mas Formularios</h1>
+      <form onSubmit={handleOnSubmitForm} className="mb-3">
+        <div className="mb-3">
+          <label htmlFor="FirstNameInput" className="form-label">
+            Nombre:
+          </label>
           <input
             type="text"
-            id="firstName"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
+            className={`form-control ${firstNameError ? "is-invalid" : ""}`}
+            
+            onChange={handleOnChangeFirstName}
           />
+          {firstNameError && <div className="invalid-feedback">{firstNameError}</div>}
         </div>
-        <div>
-          <label htmlFor="lastName">Last Name:</label>
+
+        <div className="mb-3">
+          <label htmlFor="lastNameInput" className="form-label">
+            Apellido:
+          </label>
           <input
             type="text"
-            id="lastName"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-            minLength={5}
+            className={`form-control ${lastNameError ? "is-invalid" : ""}`}
+            
+            onChange={handleOnChangeLastName}
           />
+          {lastNameError && <div className="invalid-feedback">{lastNameError}</div>}
         </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
+
+        <div className="mb-3">
+          <label htmlFor="passwordInput" className="form-label">
+            Password:
+          </label>
           <input
             type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
+            className={`form-control ${lastNameError ? "is-invalid" : ""}`}
+            
+            onChange={handleOnChangePassword}
           />
+          {passwordError && <div className="invalid-feedback">{passwordError}</div>}
         </div>
-        <div>
-          <label htmlFor="confirmPassword">Confirm Password:</label>
+
+        <div className="mb-3">
+          <label htmlFor="passwordInput" className="form-label">
+            Confirm Password:
+          </label>
           <input
             type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
+            className={`form-control ${lastNameError ? "is-invalid" : ""}`}
+            
+            onChange={handleOnChangeConfPassword}
           />
+          {confpasswordError && <div className="invalid-feedback">{confpasswordError}</div>}
         </div>
-      </form>      
+        
+       
+      </form>
+     
     </div>
   );
 };
